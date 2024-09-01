aa1930fedecb84ec90f01f1e80b666f0246fae0e
Alex Ricardo Castañeda Rodríguez <160200107+ricardious@users.noreply.github.com>
2024-08-31 20:07:50 -0600

    Revert "prueba"

--- src/main/java/com/ricardious/Main.java
+++ src/main/java/com/ricardious/Main.java
@@ -2,7 +2,6 @@ package com.ricardious;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Perdi en el torneo de café");
+        System.out.println("Hello world!");
     }
 }
-

5f0947e018f29d4814cb32242675f4b24a4a418f
AlekSCA17 <157769430+AlekSCA17@users.noreply.github.com>
2024-09-10 02:05:02 -0600

    titulo

--- src/main/java/com/ricardious/Main.java
+++ src/main/java/com/ricardious/Main.java
@@ -1,40 +1,62 @@
-package com.ricardious;
+    package com.ricardious;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-import javafx.stage.StageStyle;
+    import BDT.DatabaseConnection;
+    import javafx.application.Application;
+    import javafx.fxml.FXMLLoader;
+    import javafx.scene.Parent;
+    import javafx.scene.Scene;
+    import javafx.stage.Stage;
+    import javafx.stage.StageStyle;
+    import java.sql.Connection;
+    import java.sql.SQLException;
 
-public class Main extends Application {
-    private double xOffset = 0;
-    private double yOffset = 0;
 
-    @Override
-    public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/login.fxml"));
 
-        Scene scene = new Scene(root);
+    public class Main extends Application {
+        private double xOffset = 0;
+        private double yOffset = 0;
 
-        stage.setScene(scene);
-        stage.initStyle(StageStyle.TRANSPARENT);
+        @Override
+        public void start(Stage stage) throws Exception {
+            Parent root = FXMLLoader.load(getClass().getResource("/fxml/login.fxml"));
 
-        root.setOnMousePressed(event -> {
-            xOffset = event.getSceneX();
-            yOffset = event.getSceneY();
-        });
+            Scene scene = new Scene(root);
 
-        root.setOnMouseDragged(event -> {
-            stage.setX(event.getScreenX() - xOffset);
-            stage.setY(event.getScreenY() - yOffset);
-        });
+            stage.setScene(scene);
+            stage.initStyle(StageStyle.TRANSPARENT);
 
-        stage.setScene(scene);
-        stage.show();
+            root.setOnMousePressed(event -> {
+                xOffset = event.getSceneX();
+                yOffset = event.getSceneY();
+            });
+
+            root.setOnMouseDragged(event -> {
+                stage.setX(event.getScreenX() - xOffset);
+                stage.setY(event.getScreenY() - yOffset);
+            });
+
+            stage.setScene(scene);
+            stage.show();
+        }
+
+        public static void main(String[] args) {
+            launch(args);
+            // Intentar obtener una conexión a la base de datos
+          /*  try {
+                // Llamamos al método getConnection de DatabaseConnection
+                Connection connection = DatabaseConnection.getConnection();
+
+                // Si la conexión es exitosa, imprimimos un mensaje de éxito
+                if (connection != null) {
+                    System.out.println("La conexión a la base de datos fue exitosa.");
+                } else {
+                    System.out.println("No se pudo establecer conexión a la base de datos.");
+                }
+
+            } catch (SQLException e) {
+                System.out.println("Ocurrió un error al conectar a la base de datos.");
+                e.printStackTrace();
+            }*/
+        }
+
     }
-
-    public static void main(String[] args) {
-        launch(args);
-    }
-}

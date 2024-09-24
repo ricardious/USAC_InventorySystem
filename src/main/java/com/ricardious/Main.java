8a4c33d1742f8c78fcb1128c77161c29295beb47
Andreé Paniagua <3060027950302@ingenieria.usac.edu.gt>
2024-09-23 22:51:47 -0600

    Negreberth arreglado

--- src/main/java/com/ricardious/Main.java
+++ src/main/java/com/ricardious/Main.java
@@ -14,7 +14,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/login.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/dashboard.fxml"));
 
         Scene scene = new Scene(root);
 

4e5055157554df21a27eba24cc556629efd318bf
Alex Ricardo Castañeda Rodríguez <160200107+ricardious@users.noreply.github.com>
2024-11-14 22:39:31 -0600

    Revert "Db online"

--- src/main/java/com/ricardious/database/DatabaseConnection.java
+++ src/main/java/com/ricardious/database/DatabaseConnection.java
@@ -1,7 +1,5 @@
 package com.ricardious.database;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.sql.Statement;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 
@@ -9,15 +7,14 @@ public class DatabaseConnection {
     public Connection databaseLink;
 
     public Connection getConnection() {
-        String databaseName = "sql5744925";
-        String databaseUser = "sql5744925";
-        String databasePassword = "VCRQvDLqX4";
-        String url = "jdbc:mysql://sql5.freesqldatabase.com/" + databaseName;
+        String databaseName = "usac_inventory";
+        String databaseUser = "root";
+        String databasePassword = "root";
+        String url = "jdbc:mysql://localhost/" + databaseName;
 
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            databaseLink = DriverManager.
-                    getConnection(url, databaseUser, databasePassword);
+            databaseLink = DriverManager.getConnection(url, databaseUser, databasePassword);
         } catch (Exception e) {
             e.printStackTrace();
             e.getCause();
